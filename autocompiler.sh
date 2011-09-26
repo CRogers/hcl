c06def510c7c4e98ab70fadcf1cde1ed1d63447e
Callum Rogers <callumrogers@hotmail.co.uk>
2011-09-27 00:14:19 +0100

    Added and gate

--- autocompiler.sh
+++ autocompiler.sh
@@ -4,4 +4,4 @@
 
 filewatcher "src/" "*.coffee" "coffee -b -o bin/ -c :path" "rm lib/:wefile.js" &
 filewatcher "src/tests/" "*.coffee" "coffee -b -o tests/ -c :path" "rm tests/:wefile.js" &
-filewatcher "src/chips/" "*.coffee" "coffee -b -o chips/ -c :path" "rm chips/:wefile.js"
+filewatcher "src/chips/" "*.coffee" "coffee -b -s -p <:path >chips/:wefile.json" "rm chips/:wefile.json"
---
+++ src/chips/and.coffee
@@ -0,0 +1,9 @@
+{
+	name: 'and'
+	description: 'Takes two inputs and applies the boolean AND operation'
+	inputs:
+		a: 1
+		b: 1
+	outputs:
+		out: -> [@a & @b]
+}
